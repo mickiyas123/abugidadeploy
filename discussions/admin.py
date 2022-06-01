a1d6e0d40bd0cff489b5f32bd703b15580ad3c02
mickiy123 <mikikinfe345@gmail.com>
2022-06-01 11:38:04 +0300

    Remove all files

--- discussions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Room, Topic, Questions, Answers
-
-admin.site.register(Topic)
-admin.site.register(Room)
-admin.site.register(Questions)
-admin.site.register(Answers)
